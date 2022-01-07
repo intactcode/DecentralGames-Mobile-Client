551cca7072bd4b53407a5aaa5870b869ee97312e
Steve Becerra <33670914+mobiman1@users.noreply.github.com>
2022-01-07 13:06:46 +0700

    added pages

--- components/common/Images.tsx
+++ components/common/Images.tsx
@@ -1,6 +1,6 @@
-const Images = {
+const images = {
   SOCIAL_SHARE:
     'https://res.cloudinary.com/dnzambf4m/image/upload/c_scale,w_210,q_auto:good/v1624402416/all-dg_f5sknc.png',
 };
 
-export default Images;
+export default images;
