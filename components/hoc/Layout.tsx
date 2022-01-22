585c236f6de13f80c20ae4653b0c4415d8cd9ee9
Scott De Taboada <9022895+scottdet@users.noreply.github.com>
2022-01-22 19:32:25 +0100

    add logo

--- components/hoc/Layout.tsx
+++ components/hoc/Layout.tsx
@@ -3,9 +3,11 @@ import Logo from './Logo';
 import Hamburger from './Hamburger';
 import styles from '../../styles/Home.module.css';
 
+
 const Layout = (props: { children: any }) => {
   return (
     <Box style={{ position: 'relative', overflowX: 'hidden' }}>
+      <Logo />
       <Hamburger />
 
       <div className={styles.container}>{props.children}</div>
