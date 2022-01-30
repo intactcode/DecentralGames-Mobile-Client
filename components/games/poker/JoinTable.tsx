f3bbb6e9fbaf0bf984f245453810acdcfb19336b
Steve Becerra <33670914+mobiman1@users.noreply.github.com>
2022-01-30 08:28:09 +0700

    poker game check

--- components/games/poker/JoinTable.tsx
+++ components/games/poker/JoinTable.tsx
@@ -2,22 +2,35 @@ import { useEffect } from 'react';
 import { Box } from '@mui/material';
 // import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useStoreState } from '../../../store/Hooks';
+import { useStoreDispatch } from '../../../store/Hooks';
 import ButtonLogin from '../../buttons/ButtonLogin';
 import styles from '../../../styles/Home.module.css';
-import { useStoreState } from '../../../store/Hooks';
 
 const JoinTable = () => {
   const state = useStoreState(); // returns current state from Context API store
+  const dispatch = useStoreDispatch(); // returns dispatch method from Context API store
   const router = useRouter();
 
   /////////////////////////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
+    if (state.userStatus >= 4) {
+      console.log('Joining poker');
+
+      dispatch({
+        type: 'game',
+        data: 'poker',
+      });
+    }
+  }, [state.userStatus, dispatch]);
+
+  useEffect(() => {
     if (Object.keys(state.socket).length !== 0) {
       router.push('/poker');
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state.socket]);
+  }, [state.userStatus, state.socket]);
 
   return (
     <main className={styles.main}>
--- socket/Socket.tsx
+++ socket/Socket.tsx
@@ -11,82 +11,84 @@ const Socket = () => {
   /////////////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
     if (state.userStatus >= 4) {
-      const authToken: string = localStorage.getItem('token') || '';
-      const client = new Colyseus.Client(mobileServerURL);
+      if (state.game === 'poker') {
+        const authToken: string = localStorage.getItem('token') || '';
+        const client = new Colyseus.Client(mobileServerURL);
 
-      client
-        .joinOrCreate('pokerTable', { authToken, chips: 1000 })
-        .then((room) => {
-          console.log(`Successfuly joined ${room.name} [${room.sessionId}]`);
+        client
+          .joinOrCreate('pokerTable', { authToken, chips: 1000 })
+          .then((room) => {
+            console.log(`Successfuly joined ${room.name} [${room.sessionId}]`);
 
-          // dispatch socket instance to global state
-          dispatch({
-            type: 'socket_instance',
-            data: room,
-          });
-
-          room.onMessage('tableData', (data: any) => {
+            // dispatch socket instance to global state
             dispatch({
-              type: 'table_data',
-              data,
+              type: 'socket_instance',
+              data: room,
             });
-          });
 
-          room.onMessage('playerJoinTable', (data: any) => {
-            console.log('New player joined:', data.name);
-          });
-
-          room.onMessage('status', (data: any) => {
-            console.log('status: ', data);
-          });
-
-          room.onMessage('chipUpdate', (data: any) => {
-            dispatch({
-              type: 'chip_update',
-              data,
+            room.onMessage('tableData', (data: any) => {
+              dispatch({
+                type: 'table_data',
+                data,
+              });
             });
-          });
 
-          room.onMessage('currentSeat', (data: any) => {
-            dispatch({
-              type: 'current_seat',
-              data,
+            room.onMessage('playerJoinTable', (data: any) => {
+              console.log('New player joined:', data.name);
             });
-          });
 
-          room.onMessage('playerSitDown', (data: any) => {
-            dispatch({
-              type: 'player_sit_down',
-              data,
+            room.onMessage('status', (data: any) => {
+              console.log('status: ', data);
             });
-          });
 
-          room.onMessage('cards', (data: any) => {
-            dispatch({
-              type: 'cards',
-              data,
+            room.onMessage('chipUpdate', (data: any) => {
+              dispatch({
+                type: 'chip_update',
+                data,
+              });
             });
-          });
 
-          room.onMessage('waitTime', (data: any) => {
-            dispatch({
-              type: 'wait_time',
-              data,
+            room.onMessage('currentSeat', (data: any) => {
+              dispatch({
+                type: 'current_seat',
+                data,
+              });
             });
-          });
 
-          room.onMessage('started', () => {
-            dispatch({
-              type: 'wait_time',
-              data: 0,
+            room.onMessage('playerSitDown', (data: any) => {
+              dispatch({
+                type: 'player_sit_down',
+                data,
+              });
             });
+
+            room.onMessage('cards', (data: any) => {
+              dispatch({
+                type: 'cards',
+                data,
+              });
+            });
+
+            room.onMessage('waitTime', (data: any) => {
+              dispatch({
+                type: 'wait_time',
+                data,
+              });
+            });
+
+            room.onMessage('started', () => {
+              dispatch({
+                type: 'wait_time',
+                data: 0,
+              });
+            });
+          })
+          .catch((e) => {
+            console.log('JOIN ERROR', e);
           });
-        })
-        .catch((e) => {
-          console.log('JOIN ERROR', e);
-        });
+      }
     }
-  }, [dispatch, state.userStatus]);
+  }, [dispatch, state.userStatus, state.game]);
 
   return null;
 };
