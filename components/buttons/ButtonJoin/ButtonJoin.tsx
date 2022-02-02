928c39cfed1c74f1ad303331bf24d9fdb204633f
Scott De Taboada <9022895+scottdet@users.noreply.github.com>
2022-02-02 23:09:03 +0000

    ready to be merged now

---
+++ components/buttons/ButtonJoin/ButtonJoin.tsx
@@ -0,0 +1,18 @@
+import styles from './ButtonJoin.module.scss';
+import Image from 'next/image';
+import { Button } from 'semantic-ui-react';
+
+const ButtonJoin = () => {
+  return (
+    <div className={styles.container}>
+      <Button
+        className={styles.button_join}
+        href="/join-poker"
+      >
+        Join Game
+      </Button>
+    </div>
+  );
+};
+
+export default ButtonJoin;
