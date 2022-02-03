ef4dd17e0240720aa896a7c8d5342a310c038910
fazlerabi <fazle.rabbi.dev@gmail.com>
2022-02-03 14:51:01 -0500

    fix: remove logo on layout

--- components/hoc/Layout.tsx
+++ components/hoc/Layout.tsx
@@ -1,11 +1,9 @@
 import { Box } from '@mui/system';
-import Logo from './Logo';
 import styles from '../../styles/Home.module.css';
 
 const Layout = (props: { children: any }) => {
   return (
     <Box style={{ position: 'relative', overflowX: 'hidden' }}>
-      <Logo />
       <div className={styles.container}>{props.children}</div>
     </Box>
   );
