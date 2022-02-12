943436d8c86f3e77c97290b2e4c4093385fa6d60
Kyle Gusdorf <kgusdorf@gmail.com>
2022-02-11 22:08:23 -0500

    revert port

--- socket/Environment.tsx
+++ socket/Environment.tsx
@@ -2,7 +2,7 @@ const useLocalhostApis = true;
 
 const environment: any = {
   localhost: useLocalhostApis
-    ? 'ws://localhost:5001'
+    ? 'ws://localhost:2567'
     : 'wss://mobile.socket.decentral.games',
   testing: 'wss://mobile.socket.testing.decentral.games',
   // development: 'wss://mobile.socket.dev.decentral.games',
