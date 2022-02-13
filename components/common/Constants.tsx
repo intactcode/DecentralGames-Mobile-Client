997f623fe09d3f8b1c96845c69e97df7fd0fbf05
Steve Becerra <33670914+mobiman1@users.noreply.github.com>
2022-02-13 16:49:41 +0700

    updated BASE_URL

--- components/common/Constants.tsx
+++ components/common/Constants.tsx
@@ -1,6 +1,6 @@
 const constants = {
   SEGMENT_WRITE_KEY: 'pK03oncLYCxY1DJtTmnJnuwLByq2RlAb', // Segment public API key
-  BASE_URL: 'https://mobile.decentral.games',
+  BASE_URL: 'https://app.decentral.games',
   TITLE: 'Mobile ICE',
   DESCRIPTION: 'Play to earn games. Play ICE Poker, trade NFTs, and earn real money from your favorite games.',
   MATIC_RPC: {137: 'https://polygon-rpc.com'},
