a6d3cbcd0f6a91485d07c16a1e768dd36972f1cd
fazlerabi <fazle.rabbi.dev@gmail.com>
2022-02-15 07:53:03 -0500

    fix: remove unnecessary codes

--- components/games/poker/Character/Character.tsx
+++ components/games/poker/Character/Character.tsx
@@ -23,11 +23,8 @@ const results = [
 ];
 
 interface Props {
-  image: string;
   user?: any;
-  raise?: any;
   index: number;
-  onFold: any;
   items: any;
   ice?: number;
   xp?: number;
@@ -37,9 +34,7 @@ interface Props {
 }
 
 const Character: React.FC<Props> = ({
-  image,
   user,
-  raise,
   index,
   items,
   ice,
@@ -49,7 +44,6 @@ const Character: React.FC<Props> = ({
   classString,
 }) => {
   const state = useStoreState();
-  const currentSeat = state.currentSeat?.currentSeat;
   const activePlayer = state.tableData?.active;
   const isInHand = state.tableData?.isInHand ?? [];
   const winners = state.winners;
@@ -58,9 +52,6 @@ const Character: React.FC<Props> = ({
   const winnerIndex = get(winners, 'winners.0.0.0', isInHand.indexOf(true));
 
   const isWon = state.isWon;
-
-  const rpositionx = ['10px', '-40px', '-40px', '10px', '58px', '58px'];
-  const rpositiony = ['-80px', '20px', '20px', '120px', '20px', '20px'];
 
   const dealerx = ['-23px', '-20px', '-20px', '75px', '70px', '70px'];
   const dealery = ['-5px', '60px', '-20px', '60px', '-20px', '60px'];
@@ -112,28 +103,12 @@ const Character: React.FC<Props> = ({
         onClick={() => setInfoModalOpen(!infomodalopen)}
       >
         <Image
-          src={data?.image ?? `/${image}`}
+          src={data?.image ?? '/images/character.png'}
           width="60px"
           height="60px"
           alt="player-circle"
         />
       </div>
-      {raise && (
-        <>
-          <div
-            className={styles.raiseMoney}
-            style={{ left: rpositionx[index], top: rpositiony[index] }}
-          >
-            <div className={styles.raiseAmount}>{raise}</div>
-            <Image
-              src="/images/freecoin.svg"
-              width="18px"
-              height="18px"
-              alt="freecoin"
-            />
-          </div>
-        </>
-      )}
       <div
         className={styles.dealerChip}
         style={{ left: dealerx[index], top: dealery[index] }}
@@ -155,32 +130,26 @@ const Character: React.FC<Props> = ({
           </div>
         )}
       </div>
-      {!isWon &&
-        index !== currentSeat &&
-        isInHand[index] &&
-        !!state.cards.length && (
-          <div className={styles.cardBackContainer}>
-            <CardBack transform="matrix(0.99, -0.14, 0.14, 0.99, 0, 0)" />
-            <CardBack transform="matrix(0.99, 0.14, -0.14, 0.99, 0, 0)" />
-          </div>
-        )}
-      {!isWon &&
-        index === currentSeat &&
-        isInHand[index] &&
-        !!state.cards.length && (
-          <div className={styles.cardContainer}>
-            <Card
-              type={state.cards[0].suit}
-              number={state.cards[0].rank}
-              transform="matrix(0.99, -0.14, 0.14, 0.99, 0, 0)"
-            />
-            <Card
-              type={state.cards[1].suit}
-              number={state.cards[1].rank}
-              transform="matrix(0.99, 0.14, -0.14, 0.99, 0, 0)"
-            />
-          </div>
-        )}
+      {!isWon && !user && isInHand[index] && !!state.cards.length && (
+        <div className={styles.cardBackContainer}>
+          <CardBack transform="matrix(0.99, -0.14, 0.14, 0.99, 0, 0)" />
+          <CardBack transform="matrix(0.99, 0.14, -0.14, 0.99, 0, 0)" />
+        </div>
+      )}
+      {!isWon && user && isInHand[index] && !!state.cards.length && (
+        <div className={styles.cardContainer}>
+          <Card
+            type={state.cards[0].suit}
+            number={state.cards[0].rank}
+            transform="matrix(0.99, -0.14, 0.14, 0.99, 0, 0)"
+          />
+          <Card
+            type={state.cards[1].suit}
+            number={state.cards[1].rank}
+            transform="matrix(0.99, 0.14, -0.14, 0.99, 0, 0)"
+          />
+        </div>
+      )}
       {isWon && winners.cards[index] && (
         <div
           className={styles.winnerCardContainer}
