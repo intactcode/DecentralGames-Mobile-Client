1f5e9ad70173df62d1d11111ded2b308da94eb53
Steve Becerra <33670914+mobiman1@users.noreply.github.com>
2022-02-26 19:29:09 +0700

    updates

--- store/Provider.tsx
+++ store/Provider.tsx
@@ -23,7 +23,6 @@ const initialState = {
   isWon: false,
   isLoading: false,
   refreshBalances: false,
-  isLoading: false,
 };
 
 const reducer = (state: any, action: { type: any; data: any }) => {
@@ -125,12 +124,6 @@ const reducer = (state: any, action: { type: any; data: any }) => {
         ...state,
         refreshBalances: action.data,
       };
-    case 'set_is_loading': {
-      return {
-        ...state,
-        isLoading: action.data,
-      };
-    }
     default: {
       throw new Error('Wrong action type got dispatched');
     }
